d49c03ae850d48a14d73f77c983fb2c046a8bb3a
Caroline Schlickmann <carolschlickmann20@gmail.com>
2022-07-01 17:40:11 -0300

    Login

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 function showPassword() {
     const eye = document.getElementById('eye');
     const eyeSlash = document.getElementById('eye-slash');
-    const fielPassword = document.getElementById('field-password');
+    const fieldPassword = document.getElementById('field-password');
 
     if(eye.style.display === 'none') {
         eye.style.display = 'block';
         eyeSlash.style.display = 'none';
-        fielPassword.type = 'text';
+        fieldPassword.type = 'text';
     } else {
         eye.style.display = 'none';
         eyeSlash.style.display = 'block';
-        fielPassword.type = 'password';
+        fieldPassword.type = 'password';
     }
 
 };
